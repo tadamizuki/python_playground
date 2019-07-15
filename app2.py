852b8fd38edad365bac546fd0a54afa3f65a731f
tadamizuki <hydrangea0719@icloud.com>
2019-07-15 19:31:42 +0900

    Variables & Data Types

--- app2.py
+++ app2.py
@@ -1,6 +1,10 @@
 character_name = "John"
-character_age = "19"
+character_age = 50
+isMale = True
 print("There once was a man named " + character_name + ",")
-print("he was " + character_age + " years old.")
+print("he was " + str(character_age) + " years old.")
+
+character_name = "Mike"
 print("He really liked the name " + character_name + ",")
-print("but didn't like being " + character_age + ".")
+print("but didn't like being " + str(character_age) + ".")
+
