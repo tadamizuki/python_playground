3ce6bb597c6e02f9f2294fb66c700d888a279e89
tadamizuki <hydrangea0719@icloud.com>
2019-07-15 19:37:11 +0900

    Working With Strings

--- app2.py
+++ app2.py
@@ -1,10 +1,16 @@
-character_name = "John"
-character_age = 50
-isMale = True
-print("There once was a man named " + character_name + ",")
-print("he was " + str(character_age) + " years old.")
 
-character_name = "Mike"
-print("He really liked the name " + character_name + ",")
-print("but didn't like being " + str(character_age) + ".")
+print("Giraffe\Academy")
+print("Giraffe\nAcademy")
+print("Giraffe\"Academy")
 
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[3])
+print(phrase.index("a"))
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
